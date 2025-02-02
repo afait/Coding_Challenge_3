8e5b80069bb7b76e65328b6d5784fbde1029bcda
Annabelle Fait <imannaf8@gmail.com>
2025-02-01 19:00:56 -0500

    Task 2 - Modifying Customer Orders

--- cc_3.js
+++ cc_3.js
@@ -6,3 +6,12 @@ prices.push(20); // add price 20 to the array
 console.log("Add one new price", prices) // displays the prices with the additional new one
 prices.splice(0, 1) // remove price 10 from the array
 console.log("Updated prices:", prices) // displays the updated prices
+
+// Task 2 - Modifying Customer Orders
+
+let orders = [50, 80, 100, 60, 150];
+console.log("Initial Order Quantities:", orders); // Declarying array order quantities
+orders[2] = 105
+console.log("Increased Order Quantities:", orders) // Increased thrid order quantity from 100 to 105 and logged the output
+let total = orders.reduce((sum, order) => sum + order, 0);
+console.log("Total number of orders", total); // Calculated total number of orders
